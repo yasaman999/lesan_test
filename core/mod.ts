4ee4f478bbf799578c093a8bfb163324519fb5d7
fateme nabati <fatemenabati22@gmail.com>
2022-11-23 15:36:44 +0330

    set addUser act and implement its validator and fn

---
+++ core/mod.ts
@@ -0,0 +1,62 @@
+import { lesan, MongoClient, string, number, InRelation, OutRelation, object, ActFn, optional } from "https://deno.land/x/lesan@v0.0.66/mod.ts";
+const coreApp = lesan();
+const client = new MongoClient();
+await client.connect("mongodb://localhost:27017/arc");
+const db = client.database("core");
+coreApp.odm.setDb(db);
+
+const userPure = {
+    name: string(),
+    address: optional(string()),
+    age: number(),
+};
+
+const countryPure = {
+    name: string(),
+    description: string(),
+};
+
+const userInRel: Record<string, Inrelation> = {
+    country: {
+        schemaName: "country",
+        type: "one",
+    }
+};
+
+const userOutRel = {};
+
+countryInRel: Record<string, Inrelation> = {};
+
+countryOutRel: Record<string, Inrelation> = {
+    users: {
+        schemaName: "user",
+        number: 50,
+        sort: { field: "_id", order: "desc" },
+    }
+};
+
+const users = coreApp.odm.setModel("user", userPure, userInRel, userOutRel);
+const countries = coreApp.odm.setModel("country", countryPure, countryInRel, countryOutRel);
+
+coreApp.runServer({ port: 8080, typeGeneration: true, playground: false }); // ??
+
+const addUserValidator = () => {
+    return object({
+        set: object(userPure),
+        get: coreApp.schema.selectStruct<userInp>("user", { country: 1 })
+    })
+}
+
+const addUser: ActFn = async (body) => {
+    // const acts = coreApp.acts.getActsWithServices();
+    // console.log("in addUser function");
+    const createdUser = await users.insertOne(body.details.set);
+    return await users.findOne({ _id: createdUser }, body.details.get);//??
+}
+coreApp.acts.setAct({
+    type: "dynamic", // ??
+    schema: "user",
+    actName: "addUser",
+    validator: addUserValidator,
+    fn: addUser
+});
